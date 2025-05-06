d088fc5bd726a52c4bf6a058950ecd5ef977493a
Muqriz <muqrizzamri70@gmail.com>
2025-05-06 15:05:57 +0800

    Update types for sidebarMenu
    add codes

--- src/types/sidebarMenu.ts
+++ src/types/sidebarMenu.ts
@@ -11,4 +11,12 @@ export interface MenuItem {
     menuItems: MenuItem[];
   }
   
+// Interface for menu items with path information
+export interface MenuWithPath {
+  label: string;
+  id: string;
+  path: string;
+  section: string;
+}
+
   export type SidebarMenu = MenuSection[];
