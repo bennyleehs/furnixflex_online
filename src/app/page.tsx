f58597d680f9bca9167ea85b4dfb4fe3ee5e4c43
Muqriz <muqrizzamri70@gmail.com>
2025-10-28 09:52:40 +0800

    minor changes (main page)
    title and description metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import SalesCard from "@/components/SalesCard";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+    "Classypro Dashboard",
+  description: "Classy Project Marketing Dashboard",
 };
 
 export default function Home() {
